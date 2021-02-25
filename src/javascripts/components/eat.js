90ee9339c60b8964f3e99c75ce5ac18746ebf98b
Chris Meffley <cmeffley@gmail.com>
2021-02-24 22:06:45 -0500

    working buttons working

---
+++ src/javascripts/components/eat.js
@@ -0,0 +1,17 @@
+// const button = () => {
+//   let full = 100;
+//   const addToDom = '<button type="button" id="add1" class="btn btn-danger">Add</button><button type="button" id="sub1" class="btn btn-warning">Subtract</button>';
+//   document.querySelector('#eat').innerHTML = full;
+//   document.querySelector('#eat').innerHTML = addToDom;
+// };
+// const eatPoints = (e) => {
+//   let full = 100;
+//   if (e.target.id === '#add1') {
+//     full = full += 10;
+//   // } else (e.target.id === '#sub1') {
+//   //   full - 3;
+//   // };
+//   }
+// };
+
+// export { button, eatPoints };
--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -1,8 +1,26 @@
 import '../styles/main.scss';
+// import { button, eatPoints } from './components/eat';
+const addToDom = '<button type="button" id="add1" class="btn btn-danger">Add</button><button type="button" id="sub1" class="btn btn-warning">Subtract</button>';
+document.querySelector('#eat').innerHTML = addToDom;
+
+const eatPoints = (e) => {
+  console.warn(e);
+  const targetId = e.target.id;
+  let full = 100;
+  document.querySelector('#test').innerHTML = full;
+  if (targetId === '#add1') {
+    full += 10;
+  // } else (targetId === '#sub1') {
+  //   full -= 3;
+  // };
+  }
+  document.querySelector('#add1').addEventListener('click', eatPoints);
+  document.querySelector('#sub1').addEventListener('click', eatPoints);
+};
 
 const init = () => {
-  $('#app').html('<h1>HELLO! You are up and running!</h1>');
-  console.log('YOU ARE UP AND RUNNING!');
+  // button();
+  eatPoints();
 };
 
 init();
