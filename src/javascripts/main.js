8668dc27dc5d751039b44448a7f7f06ca0663d11
Chris Meffley <cmeffley@gmail.com>
2021-02-25 22:06:37 -0500

    got points to add and subtract

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -1,21 +1,25 @@
 import '../styles/main.scss';
 // import { button, eatPoints } from './components/eat';
-const addToDom = '<button type="button" id="add1" class="btn btn-danger">Add</button><button type="button" id="sub1" class="btn btn-warning">Subtract</button>';
-document.querySelector('#eat').innerHTML = addToDom;
+let full = 100;
+const eatPoints = () => {
+  document.querySelector('#eat').innerHTML = full;
+  const addToDom = `<br/><button type="button" id="add1" class="btn btn-danger">Add</button><br/>
+  <button type="button" id="sub1" class="btn btn-warning">Subtract</button>`;
+  document.querySelector('#eat').innerHTML += addToDom;
 
-const eatPoints = (e) => {
-  console.warn(e);
-  const targetId = e.target.id;
-  let full = 100;
-  document.querySelector('#test').innerHTML = full;
-  if (targetId === '#add1') {
-    full += 10;
-  // } else (targetId === '#sub1') {
-  //   full -= 3;
-  // };
-  }
-  document.querySelector('#add1').addEventListener('click', eatPoints);
-  document.querySelector('#sub1').addEventListener('click', eatPoints);
+  document.querySelector('#eat').addEventListener('click', (e) => {
+    if (e.target.id.includes('add1')) {
+      console.error('button has been clicked');
+      full += 10;
+      console.warn(full);
+    }
+    if (e.target.id.includes('sub1')) {
+      full -= 3;
+      console.warn(full);
+    }
+    document.querySelector('#eat').innerHTML += full;
+  // document.querySelector('#sub1').addEventListener('click', eatPoints);
+  });
 };
 
 const init = () => {
