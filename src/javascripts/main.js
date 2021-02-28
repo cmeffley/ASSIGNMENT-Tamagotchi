65b41086887f007bf3f8dd9bded85fe00846581e
Chris Meffley <cmeffley@gmail.com>
2021-02-28 18:40:46 -0500

    fight mvp

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -1,10 +1,12 @@
 import '../styles/main.scss';
 import eatPoints from './components/eat';
 import funPoints from './components/play';
+import strengthPoints from './components/fight';
 
 const init = () => {
   eatPoints();
   funPoints();
+  strengthPoints();
 };
 
 init();
