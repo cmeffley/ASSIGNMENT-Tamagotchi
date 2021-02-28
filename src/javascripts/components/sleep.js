6d48bfbb7a3cc2182c88213474041ec546cd253b
Chris Meffley <cmeffley@gmail.com>
2021-02-28 18:53:58 -0500

    sleep mvp

---
+++ src/javascripts/components/sleep.js
@@ -0,0 +1,38 @@
+import printToDom from '../helpers/printToDom';
+
+let energy = 50;
+
+const energyPoints = () => {
+  document.querySelector('#energy-points').innerHTML = energy;
+};
+
+const napTime = () => {
+  energy += 50;
+  if (energy > 100) {
+    energy = 100;
+  }
+  energyPoints();
+};
+
+const deepSlumber = () => {
+  energy += 60;
+  if (energy > 100) {
+    energy = 100;
+  }
+  energyPoints();
+};
+
+const sleepPoints = () => {
+  let sleepCard = '';
+  sleepCard += `<h1>PLAY</h1>
+  <div id="energy-points" class="text-center">${energy}</div>
+  <br/><button type="button" id="nap-time" class="btn btn-danger">Nap Time!</button>
+  <button type="button" id="deep-sleep" class="btn btn-warning">Sleepzzzz</button>
+  `;
+  printToDom('#sleep', sleepCard);
+
+  document.querySelector('#nap-time').addEventListener('click', napTime);
+  document.querySelector('#deep-sleep').addEventListener('click', deepSlumber);
+};
+
+export default sleepPoints;
--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -2,11 +2,13 @@ import '../styles/main.scss';
 import eatPoints from './components/eat';
 import funPoints from './components/play';
 import strengthPoints from './components/fight';
+import sleepPoints from './components/sleep';
 
 const init = () => {
   eatPoints();
   funPoints();
   strengthPoints();
+  sleepPoints();
 };
 
 init();
